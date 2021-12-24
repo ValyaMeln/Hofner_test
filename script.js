3febf9e46007b42d8bafc65446fd4f7d79a9d421
Valya Melnychuk <valyameln@gmail.com>
2021-12-24 12:28:02 +0200

    header menu popup js

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
-// function backToUp (){
-//     let button = $('.footer__btn_go_up');
-//     $(window).on ('scroll', ()=>{
-//         if ($(this).scrollTop()>=50){
-//             button.fadeIn();
-//         } else {
-//             button.fadeOut();
-//         }
-//     });
-//     button.on('click', (e) =>{
-//         e.preventDefault();
-//         $('html').animate({scrollTop:0}, 1000);
-//     })
-// }
-
 let ourAdvantagesOpen = document.querySelector('.btn_read_more__cloth');
-let ourAdvantagesPopup = document.querySelector(".modal__ourAdvantages__btn_read_more");
-let ourAdvantagesClose = document.querySelector(".modal__ourAdvantages_close");
+let ourAdvantagesPopup = document.querySelector('.modal__ourAdvantages__btn_read_more');
+let ourAdvantagesClose = document.querySelector('.modal__close');
+
+let burgerMenuOpenBtn = document.getElementById('open_burger_menu_btn');
+let headerMenuPopup = document.querySelector('.header__menu_popup_content');
+let burgerMenuCloseBtn = document.getElementById('close_burger_menu_btn');
+
 
 ourAdvantagesOpen.addEventListener("click", function (event) {
     event.preventDefault();
     ourAdvantagesPopup.classList.add("modal-content-show");
-    console.log("Клік по силці2222222");
 });
+
+burgerMenuOpenBtn.addEventListener("click", openBnt);
+
+function openBnt(event) {
+    event.preventDefault();
+    headerMenuPopup.classList.add("modal-content-show");
+}
 
 ourAdvantagesClose.addEventListener("click", function (event) {
     event.preventDefault();
     ourAdvantagesPopup.classList.remove('modal-content-show');
-    console.log("Клік по силці33333");
+
+});
+burgerMenuCloseBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    headerMenuPopup.classList.remove('modal-content-show');
+
 });
 
 window.addEventListener("keydown", function (event) {
     if (event.keyCode === 27) {
         if (ourAdvantagesPopup.classList.contains("modal-content-show")) {
             ourAdvantagesPopup.classList.remove("modal-content-show");
+        }
+    }
+})
+
+window.addEventListener("keydown", function (event) {
+    if (event.keyCode === 27) {
+        if (headerMenuPopup.classList.contains("modal-content-show")) {
+            headerMenuPopup.classList.remove("modal-content-show");
         }
     }
 })
